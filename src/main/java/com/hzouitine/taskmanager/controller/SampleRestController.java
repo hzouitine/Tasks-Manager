7ab97b39c36b969e2457bc4202e63e7c59a2d241
hzouitine <3zouitine@gmail.com>
2018-03-28 10:40:04 +0100

    Add Jasper Dependency

--- src/main/java/com/hzouitine/taskmanager/controller/SampleRestController.java
+++ src/main/java/com/hzouitine/taskmanager/controller/SampleRestController.java
@@ -1,12 +1,14 @@
 package com.hzouitine.taskmanager.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleRestController {
+
     @RequestMapping("/")
     String home(){
-        return "Hello world";
+        return "Hello";
     }
 }
